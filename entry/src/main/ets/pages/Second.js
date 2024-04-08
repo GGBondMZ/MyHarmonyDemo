f68cbc71dced9f79e3351be7b599aae485478cb0
mazhuang <mazzmail@163.com>
2024-04-08 16:45:56 +0800

    update

--- entry/src/main/ets/pages/Second.js
+++
@@ -1,9 +0,0 @@
-import router from '@ohos.router';
-let TAG = "MZLog";
-struct Second extends   {
-    constructor() { }
-    build() {
-        .height("100%");
-    }
-}
-//# sourceMappingURL=Second.js.map
